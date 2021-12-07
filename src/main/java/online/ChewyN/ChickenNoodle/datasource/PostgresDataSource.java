028ff68d42b96e0f244ff0b65c1e61e0f15e2e83
dereksavage1 <d.savag1@gmail.com>
2021-12-07 11:28:07 -0800

    migrated database to heroku

--- src/main/java/online/ChewyN/ChickenNoodle/datasource/PostgresDataSource.java
+++ src/main/java/online/ChewyN/ChickenNoodle/datasource/PostgresDataSource.java
@@ -1,21 +1,21 @@
-//package online.ChewyN.ChickenNoodle.datasource;
-//
-//import com.zaxxer.hikari.HikariDataSource;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class PostgresDataSource {
-//
-//    @Bean
-//    @ConfigurationProperties("app.datasource")
-//    public HikariDataSource hikariDataSource() {
-//        return DataSourceBuilder
-//                .create()
-//                .type(HikariDataSource.class)
-//                .build();
-//    }
-//
-//}
+package online.ChewyN.ChickenNoodle.datasource;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PostgresDataSource {
+
+    @Bean
+    @ConfigurationProperties("app.datasource")
+    public HikariDataSource hikariDataSource() {
+        return DataSourceBuilder
+                .create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+}
